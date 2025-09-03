c7d75726378be1accf4bd3742433876eaa05d3ab
sai krishna <saikrishnasprasad2642005@gmail.com>
2025-09-04 01:27:30 +0530

    requirements

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'bhehen ka soot App',
+  description: 'Created with love by team teri bhen ki chut',
+  generator: 'teri bhen ki chut',
 }
 
 export default function RootLayout({
